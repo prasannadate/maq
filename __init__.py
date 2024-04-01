2e3bea95b1892fd6cbfa83882b365e353d6eb08a
Dr. Prasanna Date <datepa@ornl.gov>
2024-04-01 16:10:06 -0400

    added src

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Adiabatic Quantum Artificial Intelligence (AQAI, pronounced "A-KAI") is a 
-repository of artificial intelligence and machine learning algorithms that run 
-on adiabatic quantum computers like D-Wave 2000Q. It also contains utility 
-algorithms like an efficient embedding algorithm for embedding quadratic 
-unconstrained binary optimization (QUBO) problems on D-Wave quantum computers.
-
-"""
